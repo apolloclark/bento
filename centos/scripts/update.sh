01e53070cf8481f7e92ea153a6b77819e46c33bb
apolloclark <apolloclark@gmail.com>
2018-02-22 10:03:02 -0500

    modified CentOS script to check for RHEL username and password

--- centos/scripts/update.sh
+++ centos/scripts/update.sh
@@ -1,5 +1,31 @@
 #!/bin/sh -eux
 
-yum -y update;
-reboot;
-sleep 60;
+# get the REDHAT variables
+if [ -z "$REDHAT_USER" ]; then
+    echo "ERROR: Missing <REDHAT_USER> env var"
+    exit 1
+fi
+
+if [ -z "$REDHAT_PASS" ]]; then
+    echo "ERROR: Missing <REDHAT_PASS> env var"
+    exit 1
+fi
+
+# register with Redhat
+subscription-manager register --username "$REDHAT_USER" --password "$REDHAT_PASS"
+
+# refresh
+subscription-manager refresh
+
+# register with self-serve virtual subscription
+subscription-manager subscribe --pool=8a85f98c615810120161582177020497
+
+# update system
+yum update-minimal --security -y -e 0
+
+# remove subscription
+subscription-manager remove --all
+subscription-manager unregister
+subscription-manager clean
+
+systemctl reboot
